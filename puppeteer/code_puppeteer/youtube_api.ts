589f41687de3d2829b30c54c8ca91c39389faf0f
Tom <tom@metacoders.dev>
2022-09-09 15:04:10 +0700

    fix field

--- puppeteer/code_puppeteer/youtube_api.ts
+++ puppeteer/code_puppeteer/youtube_api.ts
@@ -12,43 +12,61 @@ const youtube = google.youtube({
 });
 async function main() {
 
-    const site = await prisma.site.findMany({
+    const sites = await prisma.site.findMany({
         where: {
             parent_id: 1,
         },
     });
-    youtube.search.list({
-            part:'id,snippet',
-            q: 'Node.js',
-            type: 'video',
-            maxResults: 5
-        }).then(async(res:any) => {
-        if (res.data.items) {
-            //console.log(res.data.items);
-            const _FormatISO = formatISO(new Date());
-            const items = res.data.items.map((el:any) =>  {
-                    return {
-                        title: el.snippet.title,
-                        img_url: el.id.videoId,
-                        video_url: el.id.videoId,
-                        description: el.snippet.description,
-                        short_description: el.snippet.description,
-                        site_id:1,
-                        count_views:0,
-                        course_url:el.id.videoId,
-                        last_updated:_FormatISO
-                    }
+    sites.forEach((site) => {
+        youtube.search.list({
+                part:'id,snippet',
+                q: site.keywords,
+                type: 'video',
+                eventType:'none',
+                maxResults: 20
+            }).then((res1:any) => {
+            if (res1.data.items) {
+                const ids=res1.data.items.map((el:any) =>  {
+                    return el.id.videoId
                 }
-            );
-            console.log(items);
-            const courses = await prisma.course.createMany({data:items});
-            console.log(courses);
-            //console.log(dataInserts);
-        }
-    
-    })
-    .catch(error => {
-        console.error(error);
+                );
+                youtube.videos.list({
+                    part:'contentDetails,snippet, statistics',
+                    id: ids.join(','),
+                    //maxResults: 50
+                }).then(async(res:any) => {
+                        const _FormatISO = formatISO(new Date());
+                        const items = res.data.items.map((el:any) =>  {
+                            return {
+                                title: el.snippet.title,
+                                img_url: el.id,
+                                video_url: el.id,
+                                description: el.snippet.description,
+                                //short_description: el.snippet.description,
+                                site_id:site.id,
+                                course_url:el.id,
+                                last_updated: el.snippet.publishedAt,//_FormatISO,
+                                during: el.contentDetails.duration,
+                                count_views: el.statistics.viewCount,
+                                count_like: el.statistics.likeCount,
+                                count_comment: el.statistics.commentCount,
+                                channel_id: el.snippet.channelId,
+                                channel_name: el.snippet.channelTitle,
+                                //subscribers_count:
+                            }
+                        }
+                        );
+                        const courses = await prisma.course.createMany({data:items});
+                })
+                .catch(error => {
+                    console.error(error);
+                }); 
+            }
+        
+        })
+        .catch(error => {
+            console.error(error);
+        });    
     });    
     
 }
